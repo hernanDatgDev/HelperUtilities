63b77c3f577a8ff0c64645ba7ffcb655075d5a9f
hernan-abi <hernan.guardado-ext@ab-inbev.com>
2024-10-08 19:12:29 -0400

    Added initial test structure

---
+++ src/main/java/com/abi/util/DateFormatUtil.java
@@ -0,0 +1,10 @@
+package com.abi.util;
+
+public class DateFormatUtil {
+
+    public static String formatDate(String dateString) {
+        System.out.println("CALLED UTIL BEAN STATIC METHOD");
+        return "";
+    }
+
+}
