b57e4479f45be585698f0fced2e84348ff42b03c
hernan-abi <hernan.guardado-ext@ab-inbev.com>
2024-10-08 19:17:15 -0400

    updating test

--- src/main/java/com/abi/util/DateFormatUtil.java
+++ src/main/java/com/abi/util/DateFormatUtil.java
@@ -4,7 +4,7 @@ public class DateFormatUtil {
 
     public static String formatDate(String dateString) {
         System.out.println("CALLED UTIL BEAN STATIC METHOD");
-        return "";
+        return dateString.toUpperCase();
     }
 
 }
